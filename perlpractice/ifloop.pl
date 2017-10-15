25a886c2253cc41e0b6aaaed5bd62294b383bbbf
skolis <swetha.a0427@gmail.com>
2017-10-14 17:25:43 -0700

    added a if condition in if loop in perl

--- perlpractice/ifloop.pl
+++ perlpractice/ifloop.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
  
  $name = "Ali";
  $age = 10;
@@ -15,4 +15,24 @@
   print "this is the new change\n";
  }
 
+# unless
+$UL=30;
+unless ($UL < 30){
+ print "the UL is less than 30 \n";
+ }
 
+
+ use Switch;
+
+ $var = 10;
+ @array = (10, 20, 30);
+ %hash = ('key1' => 10, 'key2' => 20);
+
+ Switch($var){
+  case 10           { print "number 100\n"; next; }
+  case "a"          { print "string a" }
+  case [1..10,42]   { print "number in list" }
+  case (\@array)    { print "number in list" }
+  case (\%hash)     { print "entry in hash" }
+  else              { print "previous case not true" }
+  }
